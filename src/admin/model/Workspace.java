bc1c8f434cf9d4c73072edb0fc803ba9de6cfc4c
Ziya Mukhtarov <ziya.mukhtarov@ug.bilkent.edu.tr>
2019-05-08 02:55:56 +0300

    Added saving and loading functionality into Workspace

--- src/admin/model/Workspace.java
+++ src/admin/model/Workspace.java
@@ -1,7 +1,12 @@
 package admin.model;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,13 +15,37 @@ import java.util.function.Consumer;
 /**
  * Stores the workspace folder. If any get method is called before setting the
  * workspace, it will set the workspace automatically to
- * CURRENT_FOLDER\Workspace
+ * CURRENT_FOLDER\Workspace<br>
+ * <br>
+ * There are three directories inside the workspace directory:<br>
+ * 1) {@code exam_entries} contains all the data created by admin for exams<br>
+ * 2) {@code solutions} contains all the submitted solutions from examinees for
+ * the previously started exam<br>
+ * 3) {@code grades} contains all the data about the current grades given by
+ * admin
  * @author Ziya Mukhtarov
- * @version 01/05/2019
+ * @version 08/05/2019
  */
 public class Workspace
 {
 	private File workspace;
+	private static Workspace instance;
+
+	private Workspace()
+	{
+	}
+
+	/**
+	 * @return The instance of Workspace
+	 */
+	public static Workspace getInstance()
+	{
+		if ( instance == null)
+		{
+			instance = new Workspace();
+		}
+		return instance;
+	}
 
 	/**
 	 * Sets the workspace. If the workspace is already set, then it first moves the
@@ -26,9 +55,9 @@ public class Workspace
 	 */
 	public boolean set( File newWorkspace)
 	{
-		if ( newWorkspace.isDirectory() && newWorkspace.listFiles().length != 0)
+		if ( newWorkspace.isDirectory() && getByRelativePath( ".nito") == null)
 		{
-			// Non-empty folder. DANGER! Don't accept it for now.
+			// Non-empty folder. DANGER! Don't accept it for beta version.
 			// TODO ask whether the user is sure about it instead of ignoring
 			return false;
 		}
@@ -39,6 +68,18 @@ public class Workspace
 			moveContents( newWorkspace);
 		}
 		workspace = newWorkspace;
+
+		try
+		{
+			new File( workspace + File.separator + ".nito").createNewFile();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		load();
 		return true;
 	}
 
@@ -74,6 +115,70 @@ public class Workspace
 	}
 
 	/**
+	 * @param path The relative path to the needed folder or file, e.g. .nito;
+	 *             exam_entries\some_question
+	 * @return The file object referring to the file that was found, or null if no
+	 *         such file was found
+	 */
+	private File getByRelativePath( String path)
+	{
+		File[] files = get().listFiles();
+		for ( File file : files)
+		{
+			String pathToFile = file.getPath();
+			if ( pathToFile.lastIndexOf( path) == pathToFile.length() - path.length())
+			{
+				return file;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Saves the data into workspace
+	 */
+	public void save()
+	{
+		try
+		{
+			FileOutputStream fos = new FileOutputStream( getModelFile());
+			ObjectOutputStream oos = new ObjectOutputStream( fos);
+			oos.writeObject( Model.getInstance());
+			oos.close();
+			fos.close();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// TODO save other data
+	}
+
+	/**
+	 * Loads the data from workspace
+	 */
+	public void load()
+	{
+		try
+		{
+			FileInputStream fis = new FileInputStream( getModelFile());
+			ObjectInputStream ois = new ObjectInputStream( fis);
+			ois.readObject();
+			ois.close();
+			fis.close();
+		}
+		catch (IOException | ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// TODO save other data
+	}
+
+	/**
 	 * Recursively deletes the given directory. The directory should only contain
 	 * empty sub-directories in its entire hierarchy
 	 * @param folder The folder to recursively erase
@@ -106,23 +211,15 @@ public class Workspace
 	 * to CURRENT_FOLDER\Workspace
 	 * @return The folder for examinees inside workspace
 	 */
-	public File getExamineesFolder()
+	public File getExamEntriesFolder()
 	{
-		File folder = new File( get().getPath() + File.separator + "Examinees");
+		File folder = new File( get().getPath() + File.separator + "exam_entries");
 		folder.mkdirs();
 		return folder;
 	}
 
-	/**
-	 * If the workspace is not set before, this is going to set the workspace folder
-	 * to CURRENT_FOLDER\Workspace
-	 * @param e The examinee
-	 * @return The folder of the specified examinee inside the workspace
-	 */
-	public File getFolderOfExaminee( Examinee e)
+	public File getModelFile()
 	{
-		File folder = new File( getExamineesFolder().getPath() + File.separator + e.getName() + "#" + e.getId());
-		folder.mkdirs();
-		return folder;
+		return new File( getExamEntriesFolder() + File.separator + "model");
 	}
 }
