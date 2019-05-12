4f12bdcaa579a18c9905e930f4e8668289f34811
Alper Sari <alper.sari@ug.bilkent.edu.tr>
2019-05-12 17:58:40 +0300

    Updated the communication protocol for exam entries

--- src/examinee/model/Model.java
+++ src/examinee/model/Model.java
@@ -2,6 +2,7 @@ package examinee.model;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 import common.network.*;
 
@@ -62,7 +63,7 @@ public class Model
 	public boolean login( String name, String ip)
 	{
 		adminIP = ip;
-
+		username = name;
 		try
 		{
 			client = new Client( adminIP) {
@@ -75,7 +76,7 @@ public class Model
 
 			};
 
-			client.sendMessage( SECRET + ":::" + "name" + ":::" + name);
+			client.sendMessage( SECRET + ":::" + "name" + ":::" + username);
 			status = STATUS_CONNECTED;
 			
 			return true;
@@ -108,6 +109,19 @@ public class Model
 		return timeRemain;
 	}
 	
+	private ExamEntry searchId( String id)
+	{
+		ArrayList<ExamEntry> list = examData.getAll();
+		ExamEntry result = null;
+		
+		for( int i = 0; i < list.size(); i++)
+		{
+			if ( list.get(i).getId().equals(id) )
+				result = list.get(i);
+		}
+		return result;
+	}
+	
 	/**
 	 * Handles message according to protocol
 	 * @param msg Received message
@@ -120,8 +134,9 @@ public class Model
 		if ( parts[1].equals( "instruction"))
 		{
 			reference = new Instruction( parts[2], parts[3], parts[4], false, false);
-			examData.add( reference);
-			reference.setParent( examData);
+			ExamEntry parent = searchId( parts[5]);
+			parent.add(reference);
+			reference.setParent( parent);
 			// TODO
 		}
 		
@@ -129,16 +144,26 @@ public class Model
 		if ( parts[1].equals( "question") )
 		{
 			reference = new Question( parts[2], parts[3], parts[4], true, false);
-			examData.add( reference);
-			reference.setParent( examData);
+			ExamEntry parent = searchId( parts[5]);
+			parent.add(reference);
+			reference.setParent( parent);
 			// TODO
 		}
 		
 		// Create part, goes into Question
-		if ( parts[1].equals( "part") && reference.getId() == "question" )
+		if ( parts[1].equals( "part") )
 		{
-			ExamEntry tmp = new QuestionPart( parts[2], parts[3], parts[4], true, true);
-			reference.add( tmp);
+			reference = new QuestionPart( parts[2], parts[3], parts[4], true, true);
+			ExamEntry parent = searchId( parts[5]);
+			parent.add(reference);
+			reference.setParent( parent);
+			// TODO
+		}
+		
+		if ( parts[1].equals( "template") )
+		{
+			QuestionPart part = (QuestionPart) searchId( parts[4]);
+			part.updateSolution( parts[3]);
 			// TODO
 		}
 		
@@ -146,6 +171,7 @@ public class Model
 		if ( parts[1].equals( "exam") && reference == null )
 		{
 			examData = new ExamEntry( parts[2], parts[3], "", false, false);
+			examTitle = examData.getTitle();
 			timeTotal = Integer.parseInt(parts[4]); // Time in seconds
 			timeRemain = timeTotal;
 		}
@@ -161,30 +187,25 @@ public class Model
 		{
 			status = STATUS_SUSPENDED;
 		}
-		
-		
-		//******************************************************
-		// Test
-		//******************************************************
-		
-		examData = new ExamEntry( "123", "test", "", false, false);
-		timeTotal = 6000; // Time in seconds
-		timeRemain = timeTotal;
-		
-		reference = new Instruction( "001", "Instruction", "Don't Cheat", false, false);
-		examData.add( reference);
-		reference.setParent( examData);
-		
-		reference = new Question( "002", "Question 1", "Why are we here?", true, false);
-		examData.add( reference);
-		reference.setParent(examData);
-		
-		
-		reference.add( new QuestionPart( "101", "part 1", "test1", true, true));
-		
-		reference.add( new QuestionPart( "102", "part 2", "test2", true, true));
-		
-		//******************************************************
 	}
-
+	//******************************************************
+	// Test
+	//******************************************************
+//	examData = new ExamEntry( "123", "test", "", false, false);
+//	timeTotal = 6000; // Time in seconds
+//	timeRemain = timeTotal;
+//			
+//	reference = new Instruction( "001", "Instruction", "Don't Cheat", false, false);
+//	examData.add( reference);
+//	reference.setParent( examData);
+//			
+//	reference = new Question( "002", "Question 1", "Why are we here?", true, false);
+//	examData.add( reference);
+//	reference.setParent(examData);
+//					
+//	reference.add( new QuestionPart( "101", "part 1", "test1", true, true));		
+//	reference.add( new QuestionPart( "102", "part 2", "test2", true, true));		
+//			
+	//******************************************************
 }
+
